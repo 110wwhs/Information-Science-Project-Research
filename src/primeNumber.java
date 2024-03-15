b1f65c23d68281a0db3d3422ab95d646fffd125c
110wwhs <110wwhs@gmail.com>
2024-03-15 16:51:08 +0900

    v0.0 PokemonGame

--- src/primeNumber.java
+++ src/primeNumber.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
-class primeNumber {
+public class primeNumber {
     public static void main(String[] args) {
+        boolean isPrimeNumber = true;
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        int numberStart = scanner.nextInt();
+        int numberEnd = scanner.nextInt();
 
-        boolean isPrime = true; // 4byte == 1byte
+        if(numberStart > numberEnd){
+            int temp = numberStart;
+            numberStart = numberEnd;
+            numberEnd = temp;
+        }
 
-        if(number < 2) {
-            isPrime = false;
-        } else
-            for (int i =2; i*i <= number; i++) {
-                if (number % i == 0) {
-                    isPrime = false; // remove add operation.
-                    break;
+        for(int k = numberStart; k <= numberEnd; k++){
+            isPrimeNumber = true;
+            if(k < 2){
+                isPrimeNumber = false;
+            }else {
+                for(int i=2; i*i <= k; i=i+1){
+                    if(k % i == 0){
+                        isPrimeNumber = false;
+                        break;
+                    }
                 }
+                if(isPrimeNumber) System.out.print(k + " ");
             }
-
-        if (isPrime) { // remove == operation.
-            System.out.println("당신이 입력한 " + number + "는(은) 소수입니다.");
-        } else {
-            System.out.println("당신이 입력한 " + number + "는(은) 소수가 아닙니다.");
         }
     }
 }
