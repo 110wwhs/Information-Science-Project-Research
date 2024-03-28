13d21a9a9724783e73f927cb88fc0fb76eef731b
110wwhs <110wwhs@gmail.com>
2024-03-28 21:29:46 +0900

    v1.2 Pokemon

--- src/pokemons/PokemonGame.java
+++ src/pokemons/PokemonGame.java
@@ -5,20 +5,28 @@ public class PokemonGame {
 //        Pokemon p1 = new Pokemon();
 //        p1.info();
 
-        Pikachu pika1 = new Pikachu("Pikachu", 50);
-        Squirtle s1 = new Squirtle("Squirtle", 70);
-        Charizard c1 = new Charizard();
-        c1.setName("Charizard");
-        c1.setHp(150);
+        Pikachu pika1 = new Pikachu("Pikachu", 50, new NoFly());
+//        Squirtle s1 = new Squirtle("Squirtle", 70);
+        Charizard c1 = new Charizard("Charizard", 150, new Wings());
+//        c1.setName("Charizard");
+//        c1.setHp(150);
+//        pika1.setFlyable(new NoFly());
+//        c1.setFlyable(new Wings());
+//        c1.performFlyable();
+        pika1.performFlyable();
+
+        Roket roket = new Roket();
+        pika1.setFlyable(roket);
+        pika1.performFlyable();
 
 //        pika1.info();
 //        s1.info();
 
-        pika1.attack(s1);
-        s1.attack(pika1);
-        c1.attack();
-        c1.attack(pika1);
-        c1.attack(s1);
+//        pika1.attack(s1);
+//        s1.attack(pika1);
+//        c1.attack();
+//        c1.attack(pika1);
+//        c1.attack(s1);
 
 //        Squirtle s1 = new Squirtle("Squirtle", 70);
 //        Squirtle s2 = new Squirtle();
